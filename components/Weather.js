851cfd43cadef3787a4b84a4a9f8a203eaf1f844
Noeypatt <patnarin241240@gmail.com>
2018-09-28 18:17:27 +0700

    Checkpoint1

--- components/Weather.js
+++ components/Weather.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import Forecast from './Forecast';
 
-export default class App extends React.Component {
-    doIt = () => {
-        console.log("Hello from console")
+export default class Weather extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            forecast: {
+                main: '-', description: '-', temp: 0
+            }
+        }
     }
     render() {
         return (
             <View style={styles.container}>
-                <Text>{this.props.zipCode}</Text>
-            </View>
+                <ImageBackground source={require('../bg.jpeg')} style={styles.backdrop}>
+                <View style={styles.flexDir}>
+                        <Text>Zip code is {this.props.zipCode}.</Text>
+                        <Forecast {...this.state.forecast} />
+                    </View>
+                </ImageBackground>
+            </View >
         );
     }
 }
-
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+    container: { paddingTop: 25 },
+    backdrop: { width: '100%', height: '100%' },
+    flexDir: {flexDirection: 'row',justifyContent: 'center'},   
 });
