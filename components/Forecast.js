4e260ff1a546feb911743c339a0f0361a294328f
Noeypatt <patnarin241240@gmail.com>
2018-09-28 20:31:46 +0700

    Router

--- components/Forecast.js
+++ components/Forecast.js
@@ -3,18 +3,18 @@ import { StyleSheet, Text, View } from 'react-native';
 export default class Forecast extends React.Component {
     render() {
         return (
-        <View>
-            <Text style={styles.text2}>{this.props.main}</Text>
-            <Text style={styles.text3}>{this.props.description}</Text>
-            <Text style={styles.text4}>{this.props.temp} °C</Text>
-            <Text style={styles.text4}></Text>
-        </View>
+            <View>
+                <Text style={styles.text2}>{this.props.main}</Text>
+                <Text style={styles.text3}>{this.props.description}</Text>
+                <Text style={styles.text4}>{this.props.temp} °C</Text>
+                <Text style={styles.text4}></Text>
+            </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    text2: {textAlign: 'center', fontSize: 25},
-    text3: {textAlign: 'center', fontSize: 20},
-    text4: {textAlign: 'center', fontSize: 25},   
+    text2: { textAlign: 'center', fontSize: 25 },
+    text3: { textAlign: 'center', fontSize: 20 },
+    text4: { textAlign: 'center', fontSize: 25 },
 });
