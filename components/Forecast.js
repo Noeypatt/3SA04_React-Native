d1c5da9aca1f0221964d8cec43c9321fffced601
Noeypatt <patnarin241240@gmail.com>
2018-09-29 23:49:11 +0700

    App027

--- components/Forecast.js
+++ components/Forecast.js
@@ -4,17 +4,19 @@ export default class Forecast extends React.Component {
     render() {
         return (
             <View>
-                <Text style={styles.text2}>{this.props.main}</Text>
-                <Text style={styles.text3}>{this.props.description}</Text>
-                <Text style={styles.text4}>{this.props.temp} °C</Text>
-                <Text style={styles.text4}></Text>
+                <Text style={styles.text1}>{this.props.main}</Text>
+                <Text style={styles.text2}>{this.props.description}</Text>
+                <Text style={styles.text3}>{this.props.temp} °C</Text>
+                <Text style={styles.text4}>Patnarin 5935512027</Text>
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    text2: { textAlign: 'center', fontSize: 25 },
-    text3: { textAlign: 'center', fontSize: 20 },
-    text4: { textAlign: 'center', fontSize: 25 },
+    text1: { textAlign: 'center', fontSize: 25 },
+    text2: { textAlign: 'center', fontSize: 20 },
+    text3: { textAlign: 'center', fontSize: 25 },
+    text4: { textAlign: 'center', fontSize: 10, opacity: 1 }
+
 });
